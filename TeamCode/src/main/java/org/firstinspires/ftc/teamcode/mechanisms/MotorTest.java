7c3cea470b06bff617a7b6d9cc6192e2594fa3f9
danik <danikbabykbabyh@gmail.com>
2026-01-13 21:21:59 +0500

    Убрал не нужные компоненты

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/MotorTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/MotorTest.java
@@ -1,7 +1,6 @@
 package org.firstinspires.ftc.teamcode.mechanisms;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 public class MotorTest {
